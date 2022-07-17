a968e2cdaa6bbc090dc7e49718ba0da1dc3f5560
oybek <oybekjorayev500@gmail.com>
2022-07-17 18:16:20 +0300

    js

---
+++ js/main.js
@@ -0,0 +1,31 @@
+var ism = (' ismingizni kriting')
+
+console.log( 'salom ' + prompt ( ism) + ' qalesan')
+
+var yosh = (' yoshingizni kriting')
+
+console.log( ' sizni yoshingiz ' +  prompt ( yosh) + ' da')
+
+var misol = (' misloni yeching ' + '5+2=?')
+
+console.log( ' sizning javobingiz ' + '5+2=' + prompt(misol ) + " tog'ro javob 5+2=7 "  ) 
+
+var ayru = (' mislolni yecing ' + ' 100-20=? ')
+
+console.log( ' sizning javobingiz 100-20=' + prompt(ayru) + '. ' + " tog'ro javob 100-20=80. "   )
+
+var ayru = (' mislolni yecing ' + ' 100*20=? ')
+
+console.log( ' sizning javobingiz 100*20=' + prompt(ayru) + '. ' + " tog'ro javob 100*20=2000. "   )
+
+var ayru = (' mislolni yecing ' + ' 20/2=? ')
+
+console.log( ' sizning javobingiz 20/20=' + prompt(ayru) + '. ' + " tog'ro javob 20/2=1. "   )
+
+var foiz = (' misolni yeching ' + ' 40%3=? ')
+
+console.log( ' sizning javobingiz 40%3= ' + prompt(foiz) + '. ' + " tog'ri javob " +   ' 40%3=1 ')
+
+var javob = alert(' javoblarni korish uchun cansolni oching ')
+
+console.log(' savolarga javob bergaiz ucun raxmat ')
